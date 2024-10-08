76e8ac558d9f26f98022606d63ab252f59a4754c
Arturmi <artur.trombo@gmail.com>
2024-10-08 12:58:59 +0200

    clase

--- DIN html/formulario/ejercicio.js
+++ DIN html/formulario/ejercicio.js
@@ -1,18 +1,17 @@
 console.log("script");
 
 
-function validar(input, regExp){
+function validar(input, regExp){        
+    
+    if(regExp.test(input.value)) {
         
-        console.log("entra");
+        input.classList.add("correcto");
+        input.classList.remove("incorrecto");
         
-        if(regExp.test(input.value)) {
-            
-            input.classList.add("correcto");
-            input.classList.remove("incorrecto");
-            
-        } else if(!regExp.test(input.value)){
-            input.classList.add("incorrecto");
-            input.classList.remove("correcto");
-            
-        }  else (alert ("no funciona"));
+    } else if(!regExp.test(input.value)){
+       
+        input.classList.add("incorrecto");
+        input.classList.remove("correcto");
+        
+    }  else (alert ("no funciona"));
 }
